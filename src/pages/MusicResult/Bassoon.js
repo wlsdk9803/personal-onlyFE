d476a052541c826805d146ea6a5389c2b10ba13a
wlsdk9803 <wlsdk9803@naver.com>
2023-08-28 16:31:47 +0900

    feat: delete music player style

--- src/pages/MusicResult/Bassoon.js
+++ src/pages/MusicResult/Bassoon.js
@@ -3,7 +3,6 @@ import {
   Background,
   TextBox,
   CardWrapper,
-  MusicPlay,
 } from "../../component/Styled/index.js";
 import MusicResult1 from "../../component/MusicResult1/index.js";
 import Result2 from "../../component/Result2/index.js";
@@ -50,9 +49,9 @@ const Bassoon = () => {
           <TextBox padding="100px 0 0 24px" fontSize="17px" fontWeight="bold">
             나의 소리는?
           </TextBox>
-          <MusicPlay controls>
+          <audio controls>
             <source src="바순.mp3" type="audio/mpeg" />
-          </MusicPlay>
+          </audio>
 
           <MusicResult1
             musicName="바순"
